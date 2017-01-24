57c8a419fbd0d0cb42823b535ec13ffc3e4f9838
farahsharis <farahsharis@Farahs-MBP.attlocal.net>
2017-01-23 22:49:58 -0800

    Adding Front End Coding Project

---
+++ GithubFrontEnd/GithubFrontEnd/Scripts/home.js
@@ -0,0 +1,11 @@
+﻿angular.module('AngularGit', [])
+.controller('Home', function($scope, $http) {
+var days =7;
+var date = new Date(new Date().getTime() - (1000*60*60*24*days));
+var since = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDay();
+
+    $http.get('https://api.github.com/repos/angular/angular/issues?since='+since).
+        then(function(response) {
+            $scope.issues = response.data;
+        });
+});
